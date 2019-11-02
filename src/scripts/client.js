c7c99dd024482e63319fc28ac25cb0c129be771e
the-sides <sides798@gmail.com>
2019-11-02 10:46:12 -0500

    firebase inital queries. plz don't hack. very insecure

--- src/scripts/client.js
+++ src/scripts/client.js
@@ -1,1 +1,39 @@
 console.log('client side script. reporting for duty')
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyAlRMWhiWj6DQKxBluiHaMaxnyfc_nVq-o",
+    authDomain: "queueup-e5650.firebaseapp.com",
+    databaseURL: "https://queueup-e5650.firebaseio.com",
+    projectId: "queueup-e5650",
+    storageBucket: "queueup-e5650.appspot.com",
+    messagingSenderId: "537376993401",
+    appId: "1:537376993401:web:7157a7dae497b1973164a8",
+    measurementId: "G-8GB3Z2LLVC"
+};
+
+// Query data helper
+async function dataRequest(q){
+    let results = []
+    await q.get()
+    .then(
+        function(dataSet){
+            dataSet.forEach(element => {
+                results.push(element.data())
+            });
+        })
+    .catch(function(err){
+        console.error(err)
+    })
+    return results
+}
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+const db = firebase.firestore();
+const users = db.collection('users')
+let query = users.where("authoToken","==","H4w24jyBU2pg2XH8zP49q78T")
+
+dataRequest(query).then(data=>console.log(data))
