6ca54d66f5850358f50412c32d9bed38c8dc5227
the-sides <sides798@gmail.com>
2019-11-02 11:09:45 -0500

    seperating database script and specifying user token in url

--- router.js
+++ router.js
@@ -2,10 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('home', { title: 'Web Dev\'n' });
+router.get('/*', function(req, res, next) {
+  res.render('home', { title: 'qUp' });
 });
-
 
 router.get('/test',function(req, res, next){
   res.render('error', { title: 'testing env' });
---
+++ src/scripts/db.js
@@ -0,0 +1,35 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyAlRMWhiWj6DQKxBluiHaMaxnyfc_nVq-o",
+    authDomain: "queueup-e5650.firebaseapp.com",
+    databaseURL: "https://queueup-e5650.firebaseio.com",
+    projectId: "queueup-e5650",
+    storageBucket: "queueup-e5650.appspot.com",
+    messagingSenderId: "537376993401",
+    appId: "1:537376993401:web:7157a7dae497b1973164a8",
+    measurementId: "G-8GB3Z2LLVC"
+};
+
+// Query data helper
+async function dataRequest(q){
+    let results = []
+    await q.get()
+    .then(
+        function(dataSet){
+            dataSet.forEach(element => {
+                results.push(element.data())
+            });
+        })
+    .catch(function(err){
+        console.error(err)
+    })
+    return results
+}
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+const db = firebase.firestore();
+
+export {db, dataRequest}
