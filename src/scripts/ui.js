0f5bad6d9715b8fb5955ef25d9aa7d5a7e3077f6
the-sides <sides798@gmail.com>
2019-11-03 08:38:32 -0600

    updateUser creates new user elements

--- src/scripts/ui.js
+++ src/scripts/ui.js
@@ -1,6 +1,29 @@
 function updateUsers(data){
     console.log(`updating`, data)
+    let copy = document.getElementsByClassName('mainUser')[0].cloneNode(true)
+    copy.classList.remove('mainUser')
+    let root = document.querySelector('.partyPanel')
+
+    let avatar = root.querySelector('.avatar img')
+    let name = root.querySelector('.text .name')
+    let status = root.querySelector('.text .status')
+
+    avatar.src = data['avatar']
+    console.log(name)
+    name.textContent = data['discordName']
+    if(data['status'] !== undefined){
+        status.textContent = data['status']
+        name.classList.remove('center')
+        status.classList.remove('hide')
+    }
+    else{
+        name.classList.add('center')
+        status.classList.add('hide')
+    }
+
+    root.appendChild(copy)
 }
+
 function introduceMainUser(data){
     console.log(data)
     if(Number(data.length) === 1) data = data[0]
@@ -12,7 +35,7 @@ function introduceMainUser(data){
     let avatar = root.querySelector('.avatar img')
     let name = root.querySelector('.text .name')
     let status = root.querySelector('.text .status')
-    // data
+
 
     avatar.src = data['avatar']
     console.log(name)
